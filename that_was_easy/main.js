783fe9882860cfcfa0375e559651ed894d762799
Rachel Ginsberg <rachelg8524@gmail.com>
2016-09-25 03:49:31 +0000

    Add key binding to that was easy button

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -3,3 +3,9 @@ var thatWasEasy = new Audio ("that_was_easy.mp3"):
 thatWasEasy.play(); 
 }
 $("#easy").on("click", sayThatWasEasy); 
+$(document).keypress(delegateKeypress);
+function delegateKeypress(event) {
+    if (event.charCode == 32){
+    $("#easy").trigger("click");
+    }
+}
