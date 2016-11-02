9109f9cfff303fe17e3fb80d5149b53551873936
Rachel Ginsberg <rachelg8524@gmail.com>
2016-11-02 19:51:54 +0000

    Added jarring animation to geometric_pattern and it actually works now

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -7,17 +7,22 @@ var bVal;
 
 function setup(){
     createCanvas(480,600);
+    
+    frameRate(5);
+    
     circleDiameter = width/NUM_CIRCLES;
     circleRadius =circleDiameter/2;
-}
-
-function draw() {
+    
     rVal = 255;
     gVal = 0;
     bVal = 0;
+}
+
+function draw() {
+    
     
     var isShifted = false;
-    var y = 0;
+    var y = height;
     while (y>=0) {
         
     var x;
@@ -36,9 +41,9 @@ function draw() {
         y = y - circleRadius;
         isShifted = !isShifted;
         
-        rVal = rVal -2;
-        gVal = gVal +7;
-        bVal = bVal +3;
+        rVal = (rVal + 254) % 256;
+        gVal = (gVal +7) % 256;
+        bVal = (bVal +3) % 256;
     }
 }
 
