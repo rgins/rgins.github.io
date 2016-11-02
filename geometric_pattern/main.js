26b580f87d73708accabc0c143a56e183875cfce
Rachel Ginsberg <rachelg8524@gmail.com>
2016-11-02 19:45:56 +0000

    Add save keybinding to geometric_pattern

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -41,3 +41,11 @@ function draw() {
         bVal = bVal +3;
     }
 }
+
+function keyPressed() {
+    if (keyCode === 115 || keyCode ===83){
+        saveCanvas('geometricPattern', 'png');
+    }
+    return false;
+    
+}
